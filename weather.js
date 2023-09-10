4dd37c4d81aed52c5c66e704c7e99c996e4432e7
zokas92 <zstojicic92@gmail.com>
2023-09-10 12:08:24 +0200

    Connected to the API

---
+++ weather.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+//https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime
+export function getWeather(lat, lon, timezone ) {
+   return axios.get("https://api.open-meteo.com/v1/forecast?&hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime", {params: {
+        latitude: lat,
+        longitude: lon,
+        timezone
+
+    },
+}.then( ({data}) => {
+    return {
+        current: parseCurrentWeather(data),
+        daily: parseDailyWeather(data),
+        hourly: parseHourlyWeather(data)
+    }
+}
+)
+    )
+}
